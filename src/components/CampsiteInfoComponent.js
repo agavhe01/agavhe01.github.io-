db78ae18ecc0b7cc76d05bc1a418f83c64a691cb
Anesu Gavhera <agavhera@gmail.com>
2021-09-03 17:42:01 -0400

    React route parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -40,7 +40,7 @@ function CampsiteInfo(props) {
                 <div className = "container">
                     <div class = "row">
                         <RenderCampsite campsite = {props.campsite} />
-                        <RenderComments comments = {props.campsite.comments} />
+                        <RenderComments comments = {props.comments} />
                     </div>
                 </div>              
             );
