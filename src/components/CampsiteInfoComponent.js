0215418c85ab312fc3edfe0f2a17a5d842dea009
Anesu Gavhera <agavhera@gmail.com>
2021-09-17 15:53:47 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,10 +23,9 @@ class CommentForm extends Component{
         this.setState({rating: event.target.value});
       }
 
-    handleSubmit(event) {
-        alert(`Username: ${this.yourname.value} Rating: ${this.rating.value} Comment: ${this.comment.value}`);
+    handleSubmit(values) {
         this.toggleModal();
-        event.preventDefault();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     toggleModal() {
@@ -103,7 +102,7 @@ function RenderCampsite({campsite}) {
 }
 
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
         if(comments){
             return(
                 <div className = "col-md-5 m-1">
@@ -111,7 +110,7 @@ function RenderComments({comments}){
                     {comments.map(comment => <div key = {comment.id}><p>{comment.text}</p>
                     <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)}
 
-                    <CommentForm></CommentForm>    
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />    
                 </div>
                 
                 
@@ -139,7 +138,12 @@ function CampsiteInfo(props) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
+
                     </div>
 
                 </div>
